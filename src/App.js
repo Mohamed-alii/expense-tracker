1e2d2e0cf97356f891ebca8673bca73285a37193
Mohamed-alii <36523881+Mohamed-alii@users.noreply.github.com>
2021-08-23 20:05:04 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Expense from "./components/Expenses/Expense";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const INTIAL_EXPENSE = [
-  {title:'car' , date:new Date(2020 , 1 , 28) , amount:416 , id:1},
+  {title:'book' , date:new Date(2020 , 1 , 28) , amount:416 , id:1},
   {title:'wallet' , date:new Date(2020 , 5 , 2) , amount:123 , id:3},
-  {title:'smoking' , date:new Date(2019 , 10 , 13) , amount:743 , id:5},
-  {title:'travel' , date:new Date(2020 , 9 , 20) , amount:2000 , id:2}
+  {title:'drinks' , date:new Date(2019 , 10 , 13) , amount:743 , id:5},
+  {title:'food' , date:new Date(2020 , 9 , 20) , amount:2000 , id:2}
 ]
 
 
